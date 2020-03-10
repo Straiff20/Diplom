814afdab37dd3b77d1db64dd89118ca735db3ecc
Ilya Pliev <straiff20@gmail.com>
2020-03-10 11:52:03 +0300

    test appveyor 3

--- src/test/java/ForAPI/MethodsApi.java
+++ src/test/java/ForAPI/MethodsApi.java
@@ -14,7 +14,7 @@ public class MethodsApi {
     public static final int credit = 1;
 
     private static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://127.0.0.1")
+            .setBaseUri("http://::")
             .setPort(9999)
             .setAccept(ContentType.JSON)
             .setContentType(ContentType.JSON)
