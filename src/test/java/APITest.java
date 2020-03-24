78c24db4f83ddb72fcb4af872b39b8f84f214212
Ilya Pliev <straiff20@gmail.com>
2020-03-24 11:15:14 +0300

    `push 24.03.2020`

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -4,12 +4,20 @@ import ForAPI.MethodsApi;
 import io.qameta.allure.Issue;
 import io.restassured.matcher.ResponseAwareMatcher;
 import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class APITest {
+    @BeforeEach
+    @DisplayName("Clear base table")
+    void cleanTable() throws Exception {
+        SQLRequests.clearTables();
+    }
+
     @Issue("4")
     @ParameterizedTest
     @CsvFileSource(resources = "/RequestDataApi.csv", numLinesToSkip = 1)
